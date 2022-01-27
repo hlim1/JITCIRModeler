e505aebdc4eec211af2e52ea4a082c9d335d5228
Terrence Lim <hlim1@email.arizona.edu>
2022-01-27 14:36:22 -0700

    Fixed a minor typo.

--- GenPythonObj/IR.py
+++ GenPythonObj/IR.py
@@ -12,11 +12,11 @@ class IRGraph:
 
 class Node:
     def __init__(self):
-        self.graph_id = DEFAULT_ID
+        self.ir_id = DEFAULT_ID
         self.id = DEFAULT_ID
         self.alive = DEFAULT_ALIVE
         self.size = DEFAULT_NUM
-        self.opcode = DEFAULT_EMPY
+        self.opcode = DEFAULT_EMPTY
         self.address = DEFAULT_EMPTY
         self.edges = []
         self.added_nodes = []
