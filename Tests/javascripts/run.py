652786b7be02670558b84f9e6f0a719736229957
Terrence Lim <hlim1@email.arizona.edu>
2022-04-07 15:34:23 -0700

    Code to run test files with IR modeler.

--- Tests/javascripts/run.py
+++ Tests/javascripts/run.py
@@ -1,8 +1,13 @@
 """
+    This script automatically runs all test files in the test directory.
+
+    Author: Terrence Lim.
 """
 
+import copy
 import json
 import os, sys
+import argparse
 import subprocess
 
 # Reference: https://www.nsnam.org/wiki/HOWTO_understand_and_find_cause_of_exited_with_code_-11_errors
@@ -24,32 +29,95 @@ RETURNCODES = {
         "-15":"SIGTERM",
 }
 
-def run_test_files(arguments):
+# Command-line arguments to run IR Modeler.
+IRMODELER = [
+        "../../../pin",
+        "-t",
+        "../../IRModeler/obj-intel64/IRModeler.so",
+        "--"
+]
+
+# Path to 'irJsons' directory.
+IRJSONS_PATH = "./irJsons"
+
+def run_test_files(arguments: dict):
     """This function runs all test files with the selected JIT compiler
     system executable with IR Modeler to check that the input code has
     no bug.
+
+    args:
+        arguments (dict): dictionary holding arguments.
+
+    returns:
+        (list) list of files to skip due to error while running.
     """
    
-    cmd = arguments["executable-with-args"]
+    cmd = copy.copy(arguments["executable-with-args"])
     assert (
             os.path.exists(cmd[0])
     ), f"ERROR: Executable {cmd[0]} does not exists."
     cmd.append(None)
-    test_dir = arguments["test-directory"]
+    test_dir = copy.copy(arguments["test-directory"])
     assert (
             os.path.exists(test_dir)
     ), f"ERROR: Test directory does not exists."
     files = os.listdir(test_dir)
 
+    # Collect and hold files to skip when running with IR Modeler.
+    skip = []
+
+    print ("Running test files without IR Modeler...")
+
     for f in files:
-        if f.endswith(language-ext):
+        if f.endswith(arguments["language-ext"]):
             cmd[-1] = f"{test_dir}/{f}"
             output = subprocess.run(cmd, capture_output=True, text=True)
 
             if str(output.returncode) not in RETURNCODES:
                 print (f"Test File {f} ... PASSED")
             else:
-                print (f"Test File {f} ... FAILED with Return Code {RETURNCODES[output.returncode]}")
+                print (f"Test File {f} ... FAILED with Return Code {RETURNCODES[str(output.returncode)]}")
+                skip.append(f)
+
+    return skip
+
+def run_ir_modeler(arguments: dict, skip: list):
+    """This function runs all test files in the test directory with IR Modeler.
+
+    args:
+        arguments (dict): dictionary holding arguments.
+        skip (list): list of files to skip IR modeling.
+
+    returns:
+        None.
+    """
+
+    cmd = copy.copy(IRMODELER)
+    cmd.extend(copy.copy(arguments["executable-with-args"]))
+    cmd.append(None)
+
+    test_dir = arguments["test-directory"]
+    files = os.listdir(test_dir)
+
+    print ("Running test files with IR Modeler...")
+
+    for f in files:
+        if f.endswith(arguments["language-ext"]) and f not in skip:
+            cmd[-1] = f"{test_dir}/{f}"
+            output = subprocess.run(cmd, capture_output=True, text=True)
+            if str(output.returncode) not in RETURNCODES:
+                print (f"Test File {f} ... PASSED")
+                file_id = int(f.split('_')[1].split('.')[0])
+                file_path = f"{IRJSONS_PATH}/ir_{file_id}.json"
+                subprocess.run(['mv', './ir.json', file_path])
+            else:
+                print (f"Test File {f} ... FAILED with Return Code {RETURNCODES[str(output.returncode)]}")
+        elif f in skip:
+            print (f"Test file {F} ... SKIPPED")
+
+    os.remove("./trace.out")
+    os.remove("./data.out")
+    os.remove("./error.out")
 
 def load_json(json_file: str):
     try:
@@ -76,4 +144,7 @@ if __name__ == "__main__":
     arguments = load_json(argument_f)
 
     # Run all test files in the test directory without IR Modeler.
-    run_test_files(arguments)
+    skip = run_test_files(arguments)
+    
+    # Run all test files in the test directory with IR Modeler.
+    run_ir_modeler(arguments, skip)
